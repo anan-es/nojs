74f0c0b62e2f3204db561e5d94d3a97f4d4b9597
ananasowe <anana5@post.cz>
2023-07-06 22:01:15 +0200

    Add conditional javascript text

---
+++ form-fields-text.js
@@ -0,0 +1,17 @@
+// ---
+// ifjs-text
+// ---
+var nojs_forms;
+if (typeof nojs_forms === 'undefined') {
+    nojs_forms = document.getElementsByTagName('form');
+}
+
+for (let i = 0; i < nojs_forms.length; i++) {
+    const form = nojs_forms[i];
+    const elements = form.querySelectorAll(`[data-ifjs-text]`);
+
+    for (let j = 0; j < elements.length; j++) {
+        const element = elements[j];
+        element.textContent = element.getAttribute('data-ifjs-text');
+    }
+}
